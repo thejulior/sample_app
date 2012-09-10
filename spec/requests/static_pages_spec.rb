909c6fb2a8e05a90497564a3ffe6ff6120008c66
Julio Rodriguez <julior3@gmail.com>
2012-09-10 16:55:43 -0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,46 +1,26 @@
 require 'spec_helper'
 
 describe "Static pages" do
-
   subject { page }
-
   describe "Home page" do
     before { visit root_path }
     it { should have_selector('h1', text: 'Sample App') }
     it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
   end
-
-  describe "Help Page" do
-    it "should have the content 'Help'" do
-      visit help_path
-      page.should have_content('Help')
-    end
-    it "should have the right title" do
-      visit help_path
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
-    end
+  describe "Help page" do
+    before { visit help_path }
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
-
   describe "About page" do
-    it "should have the content 'About Us'" do
-      visit about_path
-      page.should have_content('About Us')
-    end
-    it "should have the right title" do
-      visit about_path
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
-    end
+    before { visit about_path }
+    it { should have_selector('h1', text: 'About') }
+    it { should have_selector('title', text: full_title('About Us')) }
   end
-
-  describe "Contact page "do
-    it "should have the content 'Contact'" do
-      visit contact_path
-      page.should have_content('Contact')
-    end
-    it "should have the right title" do
-      visit contact_path
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact")
-    end
+  describe "Contact page" do
+    before { visit contact_path }
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
 end
-
