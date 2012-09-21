503e5e1cc88fbea44ec87475ea4c4f2f25b3f668
Julio Rodriguez <julior3@gmail.com>
2012-09-21 15:45:58 -0400

    complete micropost add form

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -111,6 +111,13 @@ end
           end
         end
       end
+
+      describe "in the Microposts controller" do
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path )}
+        end
+      end
     end
   end
 end
